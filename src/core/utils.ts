9de829100ddd8398923ef2deac69d505b7691482
Maks Mitin <mitinmaksym@gmail.com>
2022-02-27 21:35:20 +0200

    feat: implement debounce function

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,3 +1,4 @@
+import { AppStateType, DataType } from "@/redux/initialState";
 export function capitalizeName(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -13,7 +14,7 @@ export function range(current: number, target: number) {
   });
 }
 
-export const storage = (key: string, data?: string) => {
+export const storage = (key: string, data?: AppStateType) => {
   if (!data) {
     return !!localStorage.getItem(key)
       ? JSON.parse(localStorage.getItem(key) ?? "")
@@ -22,10 +23,25 @@ export const storage = (key: string, data?: string) => {
   localStorage.setItem(key, JSON.stringify(data));
 };
 
-export const isEqual = (a: string | { [key: string]: string }, b: string | { [key: string]: string }) => {
+export const isEqual = (
+  a: string | DataType<string>,
+  b: string | DataType<string>
+) => {
   if (typeof a === "object" && typeof b === "object") {
     return JSON.stringify(a) === JSON.stringify(b);
   } else {
     return a === b;
   }
+};
+
+export const debounce = (fn: any, wait: number) => {
+  let timeout: NodeJS.Timeout;
+  return (...args: any) => {
+    const later = () => {
+      clearTimeout(timeout);
+      fn(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
 };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { debounce } from "./core/utils";
+import { storage } from "@core/utils";
+import { AppStateType } from "@/redux/initialState";
 import { rootReducer } from "./redux/rootReducer";
 import { Table } from "./components/table/Table";
 import { Formula } from "./components/formula/Formula";
@@ -9,12 +12,15 @@ import { createStore, Store } from "@core/createStore";
 import { initialState } from "./redux/initialState";
 
 const store: Store = createStore(rootReducer, initialState);
-store.subscribe((state) => {
-  localStorage.setItem("excel-state", JSON.stringify(state));
-});
+
+store.subscribe(
+  debounce((state: AppStateType) => {
+    storage("excel-state", state);
+  }, 2000)
+);
 const excel = new Excel("#app", {
   components: [Header, Toolbar, Formula, Table],
-  store: store,
+  store: store
 });
 
 excel.render();
